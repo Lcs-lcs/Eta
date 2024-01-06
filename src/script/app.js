38358e7572897822dbd087982a2727d0816e913d
lcs-lcs <474172047@qq.com>
2024-01-06 22:36:02 +0800

    这是一个测试

---
+++ src/script/app.js
@@ -0,0 +1,35 @@
+/* 通用应用功能 */
+
+var app = (function () {
+    'use strict';
+
+    var app = {};
+
+    app.bindingID = 'myBinding';
+
+    // 通用初始化函数(每页上都要调用)。
+    app.initialize = function () {
+        $('body').append(
+            '<div id="notification-message">' +
+                '<div class="padding">' +
+                    '<div id="notification-message-close"></div>' +
+                    '<div id="notification-message-header"></div>' +
+                    '<div id="notification-message-body"></div>' +
+                '</div>' +
+            '</div>');
+
+        $('#notification-message-close').click(function () {
+            $('#notification-message').hide();
+        });
+
+
+        // 初始化后，请公开通用通知函数。
+        app.showNotification = function (header, text) {
+            $('#notification-message-header').text(header);
+            $('#notification-message-body').text(text);
+            $('#notification-message').slideDown('fast');
+        };
+    };
+
+    return app;
+})();a
